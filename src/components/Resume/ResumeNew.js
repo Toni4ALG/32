da3a95c6755a97efde3a3c011a47733465b18f23
Dhia Eddine <82338185+Toni4ALG@users.noreply.github.com>
2023-05-29 05:08:34 +0100

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
+import pdf from "../../Assets/../Assets/toni.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-
-import pdfFile from "../../Assets/toni.pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `../../Assets/../Assets/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -24,7 +22,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdfFile}
+            href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -34,7 +32,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdfFile} className="d-flex justify-content-center">
+          <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
@@ -42,7 +40,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdfFile}
+            href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
